9f04994a9fc978149cd3d3c1adbef884f7cce50c
Paul Walker <paul@pwjw.com>
2019-02-16 08:49:21 -0500

    Runs up to point of no infoplist warning

--- premake5.lua
+++ premake5.lua
@@ -25,6 +25,16 @@ flags
    "StaticRuntime"
 }
 
+includedirs
+{
+   "deps/vstgui.surge"
+}
+
+defines
+{
+   "MAC=1"
+}
+
 buildoptions
 {
    "-std=c++17",
@@ -35,9 +45,33 @@ buildoptions
 files
 {
    "src/main.cpp",
+
+   -- a bit sloppy - deps are *ed in
+   "deps/vstgui.surge/vstgui/vstgui_mac.mm",
+   "deps/vstgui.surge/vstgui/vstgui_uidescription_mac.mm",
+
+   "deps/vstgui.surge/vstgui/standalone/source/*cpp",
+   "deps/vstgui.surge/vstgui/standalone/source/helpers/*cpp",
+   "deps/vstgui.surge/vstgui/standalone/source/platform/mac/*cpp",
+   "deps/vstgui.surge/vstgui/standalone/source/platform/mac/*mm",
+
+   -- This one should be sucked in but isn't so make it explicit
+   "deps/vstgui.surge/vstgui/uidescription/compresseduidescription.cpp"
+
 }
 
 links
 {
-   "CoreFoundation.framework"
+   "Accelerate.framework",
+   "ApplicationServices.framework",
+   "AudioUnit.framework",
+   "Carbon.framework",
+   "CoreAudio.framework",
+   "CoreAudioKit.framework",
+   "CoreServices.framework",
+   "CoreText.framework",
+   "Cocoa.framework",
+   "CoreFoundation.framework",
+   "OpenGL.framework",
+   "QuartzCore.framework",
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,48 @@
 #include <iostream>
 #include <iomanip>
 
-int main(int argc, char **argv)
+
+/*
+** This is copied from the vstgui standalone samples
+*/
+#include "vstgui/standalone/include/iapplication.h"
+#include "vstgui/standalone/include/iuidescwindow.h"
+#include "vstgui/standalone/include/helpers/appdelegate.h"
+#include "vstgui/standalone/include/helpers/windowlistener.h"
+
+using namespace VSTGUI::Standalone;
+using namespace VSTGUI::Standalone::Application;
+
+class VstGuiTestApp : public DelegateAdapter, public WindowListenerAdapter
 {
-    std::cout << "Hello World" << std::endl;
-}
+public:
+	VstGuiTestApp ()
+	: DelegateAdapter ({"simple_standalone", "1.0.0", "vstgui.examples.simplestandalone"})
+	{}
+	
+	void finishLaunching () override
+	{
+		UIDesc::Config config;
+		config.uiDescFileName = "Window.uidesc";
+		config.viewName = "Window";
+		config.windowConfig.title = "Sample App";
+		config.windowConfig.autoSaveFrameName = "SampleAppWindow";
+		config.windowConfig.style.border ().close ().size ().centered ();
+		if (auto window = UIDesc::makeWindow (config))
+		{
+			window->show ();
+			window->registerWindowListener (this);
+		}
+		else
+		{
+			IApplication::instance ().quit ();
+		}
+	}
+	void onClosed (const IWindow& window) override
+	{
+		IApplication::instance ().quit ();
+	}
+	
+};
+
+static Init gAppDelegate (std::make_unique<VstGuiTestApp> ());
