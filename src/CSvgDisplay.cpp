fd77cdb8efe08c7383838fe4f4ae5c473bb90bc8
Paul Walker <paul@pwjw.com>
2019-02-16 21:46:33 -0500

    Text now renders properly

--- src/CSvgDisplay.cpp
+++ src/CSvgDisplay.cpp
@@ -38,6 +38,8 @@ void CSvgDisplay::draw( VSTGUI::CDrawContext *dc )
 		if (!(shape->flags & NSVG_FLAGS_VISIBLE))
 			continue;
 
+        std::cout << "Shape ID=" << shape->id << " FT=" << (int)shape->fill.type << " ST=" << (int)shape->stroke.type << std::endl;
+
         //VSTGUI::CRect boundsRect(VSTGUI::CPoint(shape->bounds[0],shape->bounds[1]),
 //                                 VSTGUI::CPoint(shape->bounds[2]-shape->bounds[0],shape->bounds[3]-shape->bounds[1]));
         //dc->setFillColor(VSTGUI::kRedCColor);
@@ -45,9 +47,9 @@ void CSvgDisplay::draw( VSTGUI::CDrawContext *dc )
 
 
         
+        VSTGUI::CGraphicsPath *gp = dc->createGraphicsPath();
         for (auto path = shape->paths; path != NULL; path = path->next) {
-            //std::cout << "   PATH with " << path->npts << std::endl;
-            VSTGUI::CGraphicsPath *gp = dc->createGraphicsPath();
+            std::cout << "   PATH with " << path->npts << " " << shape->id << std::endl;
 
             for (auto i = 0; i < path->npts-1; i += 3) {
                 float* p = &path->pts[i*2];
@@ -56,71 +58,73 @@ void CSvgDisplay::draw( VSTGUI::CDrawContext *dc )
                     gp->beginSubpath(p[0],p[1]);
                 gp->addBezierCurve(p[2],p[3], p[4],p[5], p[6],p[7]);
             }
-            if( shape->fill.type != NSVG_PAINT_NONE )
-            {
-                if( shape->fill.type == NSVG_PAINT_COLOR )
-                {
-                    dc->setFillColor( svgColorToCColor( shape->fill.color ) );
-                    VSTGUI::CDrawContext::PathDrawMode pdm = VSTGUI::CDrawContext::kPathFilled;
-                    if( shape->fillRule == NSVGfillRule::NSVG_FILLRULE_EVENODD )
-                    {
-                        pdm = VSTGUI::CDrawContext::kPathFilledEvenOdd;
-                    }
-                    //std::cout << "PDM is " << (int)pdm << std::endl;
-                    dc->drawGraphicsPath(gp, pdm);
-                }
-                else if( shape->fill.type == NSVG_PAINT_LINEAR_GRADIENT )
-                {
-                    bool evenOdd = (shape->fillRule == NSVGfillRule::NSVG_FILLRULE_EVENODD);
-                    NSVGgradient *ngrad = shape->fill.gradient;
-                    std::cout << "Shape ID=" << shape->id << " FT=" << (int)shape->fill.type << " ST=" << (int)shape->stroke.type << std::endl;
-                    std::cout << "Gradient NPoints = " << ngrad->nstops << " FX=" << ngrad->fx << " FY=" << ngrad->fy << " spread=" << (int)ngrad->spread << std::endl;
-                    std::cout << "   xform = ";
-                    for( int i=0; i<6; ++i ) std::cout << ngrad->xform[ i ] << " ";
-                    std::cout << std::endl;
-
-                    float *x = ngrad->xform;
-                    VSTGUI::CGraphicsTransform gradXform( x[0], x[1], x[2], x[3], x[4], x[5] );
-                    VSTGUI::CGradient::ColorStopMap csm;
-                    VSTGUI::CGradient *cg = VSTGUI::CGradient::create(csm);
-                    
-                    for( int i=0; i<ngrad->nstops; ++i )
-                    {
-                        auto stop = ngrad->stops[ i ];
-                        std::cout << "   " << std::setw(8) <<  stop.offset << " " << std::hex << stop.color << std::endl;
-                        cg->addColorStop(stop.offset, svgColorToCColor(stop.color));
-                    }
-                    dc->fillLinearGradient(gp, *cg,
-                                           VSTGUI::CPoint(shape->bounds[0],shape->bounds[1]),
-                                           VSTGUI::CPoint(shape->bounds[2],shape->bounds[3]),
-                                           evenOdd );
-                    dc->setFrameColor(VSTGUI::kRedCColor);
-                    dc->drawGraphicsPath(gp,VSTGUI::CDrawContext::kPathStroked);
-                }
-                else
-                {
-                    std::cerr << "No radial gradient support yet" << std::endl;
-                    dc->setFillColor( VSTGUI::kRedCColor );
-                }
-
-            }
-            if( shape->stroke.type != NSVG_PAINT_NONE )
-            {
-                if( shape->stroke.type == NSVG_PAINT_COLOR )
-                {
-                    dc->setFrameColor( svgColorToCColor( shape->stroke.color ) );
-                }
-                else
-                {
-                    std::cerr << "No gradient support yet" << std::endl;
-                    dc->setFrameColor( VSTGUI::kRedCColor );
-                }
-
-                dc->setLineWidth(shape->strokeWidth);
-                dc->drawGraphicsPath(gp, VSTGUI::CDrawContext::kPathStroked );
-            }
-            gp->forget();
         }
+        if( shape->fill.type != NSVG_PAINT_NONE )
+        {
+            if( shape->fill.type == NSVG_PAINT_COLOR )
+            {
+                dc->setFillColor( svgColorToCColor( shape->fill.color ) );
+                std::cout << "Filling path with " << std::hex << shape->fill.color << std::endl;
+                VSTGUI::CDrawContext::PathDrawMode pdm = VSTGUI::CDrawContext::kPathFilled;
+                if( shape->fillRule == NSVGfillRule::NSVG_FILLRULE_EVENODD )
+                {
+                    pdm = VSTGUI::CDrawContext::kPathFilledEvenOdd;
+                }
+                //std::cout << "PDM is " << (int)pdm << std::endl;
+                dc->drawGraphicsPath(gp, pdm);
+            }
+            else if( shape->fill.type == NSVG_PAINT_LINEAR_GRADIENT )
+            {
+                bool evenOdd = (shape->fillRule == NSVGfillRule::NSVG_FILLRULE_EVENODD);
+                NSVGgradient *ngrad = shape->fill.gradient;
+                std::cout << "Shape ID=" << shape->id << " FT=" << (int)shape->fill.type << " ST=" << (int)shape->stroke.type << std::endl;
+                std::cout << "Gradient NPoints = " << ngrad->nstops << " FX=" << ngrad->fx << " FY=" << ngrad->fy << " spread=" << (int)ngrad->spread << std::endl;
+                std::cout << "   xform = ";
+                for( int i=0; i<6; ++i ) std::cout << ngrad->xform[ i ] << " ";
+                std::cout << std::endl;
+                
+                float *x = ngrad->xform;
+                VSTGUI::CGraphicsTransform gradXform( x[0], x[1], x[2], x[3], x[4], x[5] );
+                VSTGUI::CGradient::ColorStopMap csm;
+                VSTGUI::CGradient *cg = VSTGUI::CGradient::create(csm);
+                
+                for( int i=0; i<ngrad->nstops; ++i )
+                {
+                    auto stop = ngrad->stops[ i ];
+                    std::cout << "   " << std::setw(8) <<  stop.offset << " " << std::hex << stop.color << std::endl;
+                    cg->addColorStop(stop.offset, svgColorToCColor(stop.color));
+                }
+                dc->fillLinearGradient(gp, *cg,
+                                       VSTGUI::CPoint(shape->bounds[0],shape->bounds[1]),
+                                       VSTGUI::CPoint(shape->bounds[2],shape->bounds[3]),
+                                       evenOdd );
+                dc->setFrameColor(VSTGUI::kRedCColor);
+                dc->drawGraphicsPath(gp,VSTGUI::CDrawContext::kPathStroked);
+            }
+            else
+            {
+                std::cerr << "No radial gradient support yet" << std::endl;
+                dc->setFillColor( VSTGUI::kRedCColor );
+            }
+            
+        }
+        if( shape->stroke.type != NSVG_PAINT_NONE )
+        {
+            if( shape->stroke.type == NSVG_PAINT_COLOR )
+            {
+                dc->setFrameColor( svgColorToCColor( shape->stroke.color ) );
+            }
+            else
+            {
+                std::cerr << "No gradient support yet" << std::endl;
+                dc->setFrameColor( VSTGUI::kRedCColor );
+            }
+            
+            dc->setLineWidth(shape->strokeWidth);
+            dc->drawGraphicsPath(gp, VSTGUI::CDrawContext::kPathStroked );
+        }
+        gp->forget();
+
     }
 }
 
